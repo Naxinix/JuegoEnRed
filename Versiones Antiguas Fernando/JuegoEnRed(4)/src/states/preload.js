1edac17c28978d7a2a21db276a79a03e90f35638
Naxinix <43404918+Naxinix@users.noreply.github.com>
2018-10-23 18:48:13 +0200

    Add files via upload
    Añadidos grandes cambios:
    -Agujeros negro y efecto
    -Nave loot y efecto
    -Tres clases de naves con sus características, ultimates y habilidades implementadas
    -Sistema de salud y daño
    -Sistema de contador de ultimate
    -Sistema de disparos y colisiones
    -Nave dummy para hacer pruebas (no disponible en el producto final)

---
+++ Versiones Antiguas Fernando/JuegoEnRed(4)/src/states/preload.js
@@ -0,0 +1,42 @@
+Project.preloadState = function(game) {
+
+}
+
+Project.preloadState.prototype = {
+
+    preload: function() {
+        game.load.image('background','../assets/images/background.jpg');
+        game.load.image('play','../assets/images/play.png');
+        game.load.image('spaceship','../assets/images/spaceship.png');
+        game.load.image('assault','../assets/images/spaceship.png');
+        game.load.image('selected','../assets/images/selected.jpg');
+        game.load.image('arrow','../assets/images/arrow.png');
+        game.load.image('minimap','../assets/images/Minimap.jpg');
+        game.load.image('position','../assets/images/position.jpg');
+        game.load.image('lf1','../assets/images/lifebar1.jpg');
+        game.load.image('lf2','../assets/images/lifebar2.jpg');
+        game.load.image('bullet','../assets/images/bullet.png');
+        game.load.image('blackhole','../assets/images/blackhole.png');
+        game.load.image('blackhole2','../assets/images/blackhole2.png');
+        game.load.image('graviton','../assets/images/graviton.png');
+        game.load.image('ultimate','../assets/images/ultimate.jpg');
+        game.load.image('mark','../assets/images/mark.png');
+        game.load.image('lootShipIcon','../assets/images/lootShipIcon.png');
+        game.load.image('lootShip','../assets/images/lootShip.png');
+        game.load.image('area','../assets/images/area.png');
+        game.load.image('area2','../assets/images/area2.jpg');
+
+   
+
+
+    },
+
+    create: function() {
+        
+        this.state.start('menuState');
+    },
+
+    update: function() {
+
+    }
+}
