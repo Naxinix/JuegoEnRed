551e70ad4ffd7bb6bca15e98d39f1f74e980cdc0
DiegoKiller98 <43404893+DiegoKiller98@users.noreply.github.com>
2018-11-12 18:47:29 +0100

    Update menu.js
    Añadida una animacion de fondo, para un menu mas dinamico

--- CursedStars 1.0/src/states/menu.js
+++ CursedStars 1.0/src/states/menu.js
@@ -9,24 +9,26 @@ Project.menuState.prototype = {
     },
 
     create: function() {
-        var s = game.add.sprite(0, 0, 'background');
-        var titulo=game.add.sprite(20,20,'titulo');
+        createBackgroundAnimation();
+
+        //var s = game.add.sprite(0, 0, 'background');
+        var titulo = game.add.sprite(20,20,'titulo');
         var play = game.add.sprite(130, 300, 'seleccionclase');
         var como = game.add.sprite(130, 400, 'comojugar');
         
-        s.scale.setTo(1.5,1);
+        //s.scale.setTo(1.5,1);
         play.scale.setTo(0.7,0.7);
         play.inputEnabled = true;
         play.events.onInputDown.add(this.listener, this);
 
         como.scale.setTo(0.7,0.7);
-        como.inputEnabled=true;
+        como.inputEnabled = true;
         como.events.onInputDown.add(this.listener2, this);
 
     },
 
     update: function() {
-
+        updateAnim();
     },
     
 
