d3c24a3d17951cd8e7f383c890277c4d4e597428
Naxinix <43404918+Naxinix@users.noreply.github.com>
2018-11-25 20:44:43 +0100

    Update menu.js

--- CursedStarsBueno/src/main/resources/static/src/states/menu.js
+++ CursedStarsBueno/src/main/resources/static/src/states/menu.js
@@ -2,6 +2,30 @@ Project.menuState = function(game) {
 
 }
 
+function start(){
+    music.fadeIn(4000);
+}
+
+function overplay(){
+    button.play();
+    play2 = game.add.sprite(130, 300, 'seleccionclase');
+    play2.scale.setTo(0.7,0.7);
+}
+function overcomo(){
+    button.play();
+    como2 = game.add.sprite(130, 400, 'comojugar');
+    como2.scale.setTo(0.7,0.7);
+}
+
+function outplay(){
+	if(play2!=null)
+		play2.destroy();
+}
+function outcomo(){
+	if(como2!=null)
+		como2.destroy();
+}
+
 Project.menuState.prototype = {
 
     preload: function() {
@@ -9,24 +33,42 @@ Project.menuState.prototype = {
     },
 
     create: function() {
-        var s = game.add.sprite(0, 0, 'background');
+
+    	createBackgroundAnimation();
+    	
         var titulo=game.add.sprite(20,20,'titulo');
-        var play = game.add.sprite(130, 300, 'seleccionclase');
-        var como = game.add.sprite(130, 400, 'comojugar');
+        tituloAnim=titulo.animations.add('tituloAnim');
+        titulo.animations.play('tituloAnim',5,false);
+        var play = game.add.sprite(130, 300, 'seleccionclase_noselec');
+        var como = game.add.sprite(130, 400, 'comojugar_noselec');
+        var play2=null;
+        var como2=null;
         
-        s.scale.setTo(1.5,1);
+       // s.scale.setTo(1.5,1);
         play.scale.setTo(0.7,0.7);
         play.inputEnabled = true;
         play.events.onInputDown.add(this.listener, this);
+        play.events.onInputOver.add(overplay, this);
+        play.events.onInputOut.add(outplay,this);
 
         como.scale.setTo(0.7,0.7);
         como.inputEnabled=true;
         como.events.onInputDown.add(this.listener2, this);
+        como.events.onInputOver.add(overcomo, this);
+        como.events.onInputOut.add(outcomo, this);
+        
+        music = game.add.audio('menu');
+        if(!sonando){
+        music.onDecoded.add(start, this);
+        sonando=true;
+        }
+        //music.play();
+        button=game.add.audio('button');
 
     },
 
     update: function() {
-
+    	updateAnim();
     },
     
 
