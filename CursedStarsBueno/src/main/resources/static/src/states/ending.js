bb9e210c10731ad889d1cc2ee1ef96562b9182ff
Naxinix <43404918+Naxinix@users.noreply.github.com>
2018-11-25 20:43:05 +0100

    Update ending.js

--- CursedStarsBueno/src/main/resources/static/src/states/ending.js
+++ CursedStarsBueno/src/main/resources/static/src/states/ending.js
@@ -8,17 +8,24 @@ Project.endingState.prototype = {
         
     },
 
-    create: function() {
-    
-    	//deletePlayer(1);
-    	//deletePlayer(2);
+    create: function() {/*
+    if(Project.player1!=undefined){
+    deletePlayer(Project.player1.id);
+    delete Project.player1;
+    }
+    	*/
     var s = game.add.sprite(0, 0, 'background');
     s.scale.setTo(1.5,1);
+  //  var sonidofinal;
 
    if(!win){
    var gameover = game.add.sprite(40, 50, 'gameover');
+   sonidofinal=game.add.audio('derrota');
+   sonidofinal.play();
    }else{
    var victoria = game.add.sprite(40, 50, 'victoria');   
+   sonidofinal=game.add.audio('victoria');
+   sonidofinal.play();
    }
    var reintentar=game.add.sprite(270,250,'reintentar');
    var salir=game.add.sprite(270, 350, 'salir');
@@ -31,6 +38,8 @@ Project.endingState.prototype = {
 
    reintentar.events.onInputDown.add(this.listener2, this);
    salir.events.onInputDown.add(this.listener, this);
+   
+
 
     },
 
@@ -40,10 +49,12 @@ Project.endingState.prototype = {
     
 
     listener: function(){
+    	sonidofinal.stop();
         this.state.start('menuState');
     },
 
     listener2: function(){
+    	sonidofinal.stop();
         this.state.start('chooseState');
     }
     
