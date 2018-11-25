a3e1cc5b81fc6531d5e941290c574af942b12033
Naxinix <43404918+Naxinix@users.noreply.github.com>
2018-11-25 20:39:47 +0100

    Update boot.js

--- CursedStarsBueno/src/main/resources/static/src/states/boot.js
+++ CursedStarsBueno/src/main/resources/static/src/states/boot.js
@@ -7,7 +7,7 @@ Project.bootState = function(game) {
 Project.bootState.prototype = {
 
     preload: function() {
-        
+        game.load.spritesheet('loading','../assets/images/interface/loadingScreenSpriteSheet1.png',400,225,6);
     },
 
     create: function() {
