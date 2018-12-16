43b5cf6811fb9012eecb7372d57f818c131924fe
DiegoKiller98 <43404893+DiegoKiller98@users.noreply.github.com>
2018-12-16 17:34:42 +0100

    Update choose.js

--- CursedStarsBueno/src/main/resources/static/src/states/choose.js
+++ CursedStarsBueno/src/main/resources/static/src/states/choose.js
@@ -3,7 +3,7 @@ Project.chooseState = function(game) {
 }
 var d;
 function p(pointer) {
-    console.log(pointer.event);
+    //console.log(pointer.event);
 }
 
 Project.chooseState.prototype = {
@@ -16,7 +16,7 @@ Project.chooseState.prototype = {
         classSelected = 0;
         classSelected2 = 0;
         var s = game.add.tileSprite(0, 0,800,600, 'background');
-      //  s.scale.setTo(1.5,1);
+        //s.scale.setTo(1.5,1);
         var t = game.add.sprite(40,0,'select');
         disrupterInfo=game.add.sprite(230, 100, 'disrupterinfo');
         game.physics.enable(disrupterInfo, Phaser.Physics.ARCADE);
